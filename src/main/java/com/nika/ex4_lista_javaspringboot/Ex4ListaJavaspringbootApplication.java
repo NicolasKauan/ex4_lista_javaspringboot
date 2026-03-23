6e08059a0d7cb369047e7104a40867e0ccf7bd3c
Nicolas Kauan <166877999+NicolasKauan@users.noreply.github.com>
2026-03-22 23:53:16 -0300

    first commit

---
+++ src/main/java/com/nika/ex4_lista_javaspringboot/Ex4ListaJavaspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.nika.ex4_lista_javaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex4ListaJavaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex4ListaJavaspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nika/ex4_lista_javaspringboot/Ex4ListaJavaspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nika.ex4_lista_javaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex4ListaJavaspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
